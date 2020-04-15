5e937ff04dac5a4455f1a0c5a9a4ca292bb26d7b
fearlessxxx <41656402+fearlessxxx@users.noreply.github.com>
2020-04-15 22:06:21 +0800

    Update all_prime_number.py

--- all_prime_number.py
+++ all_prime_number.py
@@ -7,3 +7,6 @@ def all_prime_number(n):
                 list_original[j] = 0 #如果列表中的值不为0，则把大于该值切为其倍数的元素置为0
                 j += i
     return [i for i in list_original[2:] if i != 0] #返回原始列表中不为0的数
+'''
+时间复杂度O(N)
+空间复杂度O(N)
